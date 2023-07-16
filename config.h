9135fe1f2c5982c2b6335c8549a4272edcf1053e
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-16 18:34:06 +0700

    update size font

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=10:antialias=true","Roboto Regular:size=10:antialias=true", "JetbrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true"};
-static const char dmenufont[]       = "JetbrainsMono Nerd Font:size=10:antialias=true";
+static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=12:antialias=true", "JetbrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true"};
+static const char dmenufont[]       = "JetbrainsMono Nerd Font:size=12:antialias=true";
 static char normbgcolor[]           = "#1D2021";
 static char normbordercolor[]       = "#3C3836";
 static char normfgcolor[]           = "#EBDBB2";
